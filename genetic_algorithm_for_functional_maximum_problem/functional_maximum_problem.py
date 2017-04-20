69146a7aec439f75b59d8ba00a8d1b36defed441
he <ffpphh@outlook.com>
2017-04-21 00:44:08 +0800

    遗传算法基本完成

--- genetic_algorithm_for_functional_maximum_problem/functional_maximum_problem.py
+++ genetic_algorithm_for_functional_maximum_problem/functional_maximum_problem.py
@@ -1,8 +1,6 @@
 import math
 import random
 
-print(int('000000000000000000000001',2))
-
 class genetic_algorithm(object):
 
     def __init__(self, population_size, string_len, pc, pm):
@@ -18,15 +16,16 @@ class genetic_algorithm(object):
         self.pc = pc
         self.pm = pm
 
-    def convert_to_decimal(binary_figure):
+    def convert_to_decimal(self,binary_figure):
+        # print(binary_figure + '  len:' + str(math.pow(2, len(binary_figure))))
         '''把二进制数转化为【0,9】的数'''
         decimal_figure = int(binary_figure,2)*9/math.pow(2, len(binary_figure))
         return decimal_figure
 
 
     def calc_fitness(self, val):
-        x = convert_to_decimal(val)
-        y = x + 10 * math.sin(5 * x) + 7 * math.cosh(4 * x)
+        x = self.convert_to_decimal(val)
+        y = x + 10 * math.sin(5 * x) + 7 * math.cos(4 * x)
         return y
 
     def crossover(self, chromosome1, chromosome2):
@@ -35,27 +34,27 @@ class genetic_algorithm(object):
             end = random.randint(start, self.chromosome_len)
             tmp_str1 = chromosome1[start:end]
             tmp_str2 = chromosome2[start:end]
-            for i in range(end - start):
-                chromosome1[start + i] = tmp_str2[i]
-                chromosome2[start + i] = tmp_str1[i]
-            
+            chromosome1 = chromosome1[0:start] + tmp_str2 + chromosome1[end:]
+            chromosome2 = chromosome2[0:start] + tmp_str1 + chromosome2[end:]
         return [chromosome1,chromosome2]
 
 
     def mutation(self,chromosome):
         if random.random() < self.pm:
-            times = random.randint(10)
+            times = random.randint(0, 10)
             for i in range(times):
-                index = random.randint(self.chromosome_len)
+                index = random.randint(0, self.chromosome_len - 1)
+                new_chromosome = chromosome[0:index]
                 if chromosome[index] == '1':
-                    chromosome[index] = '0'
+                    new_chromosome += '0'
                 else:
-                    chromosome[index] = '0'
+                    new_chromosome += '1'
+                chromosome = new_chromosome + chromosome[index+1:]
         return chromosome
 
 
 
-    def genetic_fun(self,population_size, string_len, pc, pm):
+    def genetic_fun(self):
         '''
         遗传函数
         population_size：种群规模
@@ -65,25 +64,24 @@ class genetic_algorithm(object):
         '''
         population = []
 
-        for i in range(population_size):
-            classmethod = ''
-            for j in range(0, string_len):
+        for i in range(self.population_size):
+            chromosome = ''
+            for j in range(self.chromosome_len):
                 chromosome = chromosome + random.choice('01')
             population.append(chromosome)
-        
-        fitness_sum = sum(chromosomes_fitness)
+
 
         count = 0
-        while count < 40:
+        while count < 400:
+
             chromosomes_fitness = []
-
             for chromosome in population:
-                chromosomes_fitness.append(calc_fitness(chromosome))
+                chromosomes_fitness.append(self.calc_fitness(str(chromosome)))
 
-            fitness_sum = sum(chromosomes_fitness)
+            fitness_sum = int(sum(chromosomes_fitness))
 
             population_new = []
-            while len(population_new) < population_size:
+            while len(population_new) < self.population_size:
                 random1 = random.randint(0, fitness_sum - 1)
                 random2 = random.randint(0, fitness_sum - 1)
 
@@ -92,20 +90,32 @@ class genetic_algorithm(object):
 
                 tmp = 0
                 for i in range(len(population)):
-                    if random1 >= tmp && random1 < tmp + chromosomes_fitness[i]:
+                    if random1 >= tmp and random1 < tmp + chromosomes_fitness[i]:
                         chromosome1 = population[i]
-                    if random2 >= tmp && random2 < tmp + chromosomes_fitness[i]:
+                    if random2 >= tmp and random2 < tmp + chromosomes_fitness[i]:
                         chromosome2 = population[i]
                     tmp += chromosomes_fitness[i]
                 pass
 
-                population_new.append(crossover(chromosome1, chromosome2))
-                population_new.append(mutation(chromosome1))
-                population_new.append(mutation(chromosome2))
+                crossover_chromosome = self.crossover(chromosome1, chromosome2)
+                chromosome1 = crossover_chromosome[0]
+                chromosome2 = crossover_chromosome[1]
+                population_new.append(self.mutation(chromosome1))
+                population_new.append(self.mutation(chromosome2))
             pass
 
             population = population_new
             count += 1
         pass
 
+        chromosomes_fitness = []
+        for chromosome in population:
+            chromosomes_fitness.append(self.calc_fitness(chromosome))
+        
+        return max(chromosomes_fitness)
 
+def main():
+    test = genetic_algorithm(500, 24, 0.6, 0.01)
+    result = test.genetic_fun()
+    print(result)
+main()
--- genetic_algorithm_for_functional_maximum_problem/test.py
+++ genetic_algorithm_for_functional_maximum_problem/test.py
@@ -1,11 +1,13 @@
+import math
 aa = '123456'
 bb = 'qwerty'
 
 
 def test(val):
     val += 1
-
+a,b = [0,1]
 val = 0
 print(val)
 test(val)
-print(val)
+print(a)
+print(int('111111101110010100111011',2)*9/math.pow(2,24))
