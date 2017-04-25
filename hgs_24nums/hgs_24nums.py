2201fad16afb22532c42cf6d4196f52fbd03c069
he <ffpphh@outlook.com>
2017-04-25 22:00:06 +0800

    改进，简单例子可以

--- hgs_24nums/hgs_24nums.py
+++ hgs_24nums/hgs_24nums.py
@@ -1,5 +1,6 @@
 import numpy as np
-import hgs_24nums_node as node
+from hgs_24nums_node import HGS24NumsNode as Node
+import random
 
 open_list = []
 closed_list = []
@@ -7,60 +8,141 @@ start_nums_table = ''
 end_nums_table = ''
 
 
+def node_compare(x):
+    return x.score
+    # if x.score < y.score:
+    #     return -1
+    # if x.score >= y.score:
+    #     return 1
+def hgs(start_nums_table,end_nums_table):
+    # start_nums_table = np.array(
+    #     [[0,0,0,0,0],
+    #     [0,0,0,0,0],
+    #     [0,0,0,0,0],
+    #     [0,0,0,0,0],
+    #     [0,0,0,0,-1]]
+    # )
+    # end_nums_table = np.array(
+    #     [[0,0,0,0,0],
+    #     [0,0,0,0,0],
+    #     [0,0,-1,0,0],
+    #     [0,0,0,0,0],
+    #     [0,0,0,0,0]]
+    # )
+    Node.start_nums_table = start_nums_table
+    Node.end_nums_table = end_nums_table
 
-def main():
-    start_nums_table = np.array(
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,-1]
-    )
-    end_nums_table = np.array(
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,-1],
-        [0,0,0,0,0]
-    )
-    hgs_24nums_node.start_nums_table = start_nums_table
-    hgs_24nums_node.end_nums_table = end_nums_table
-
-    start_node = hgs_24nums_node(start_nums_table,0)
+    start_node = Node(start_nums_table,0)
     open_list.append(start_node)
 
     while True:
         if len(open_list) == 0:
+            print('False')
             return False
         
         node_n = open_list.pop(0)
         closed_list.append(node_n)
 
         if (node_n.nums_table == end_nums_table).all():
+            print(node_n.move_count)
+            print('true')
             return True
         
-        node_n_move = [Node(node_n),Node(node_n),Node(node_n),Node(node_n)]
-        move_succeed = []
-        move_succeed.append(node_n_move[0].blank_up())
-        move_succeed.append(node_n_move[1].blank_down())
-        move_succeed.append(node_n_move[2].blank_right())
-        move_succeed.append(node_n_move[3].blank_left())
+        node_n_move = []
+        node_n_move0 = Node(node_n)
+        node_n_move1 = Node(node_n)
+        node_n_move2 = Node(node_n)
+        node_n_move3 = Node(node_n)
 
+        if node_n_move0.blank_up():
+            node_n_move.append(node_n_move0)
+        if node_n_move1.blank_down():
+            node_n_move.append(node_n_move1)
+        if node_n_move2.blank_right():
+            node_n_move.append(node_n_move2)
+        if node_n_move3.blank_left():
+            node_n_move.append(node_n_move3)
+        no_exist_per = []
         for i in range(len(node_n_move)):
-            if move_succeed[i]:
-                no_exist = True
-                node_tmp = node_n.last_node
-                while True:
-                    if node_tmp.nums_table == 0:
+            no_exist_per.append(True)
+        
+        node_tmp = node_n.last_node
+        while True:
+            if node_tmp == None:
+                break
+            for i in range(len(node_n_move)):
+                if (node_tmp.nums_table == node_n_move[i].nums_table).all():
+                    no_exist_per[i] = False
+            node_tmp = node_tmp.last_node
+            
+        for i in range(len(node_n_move)):
+            if no_exist_per[i]:
+                node_tmp = node_n_move[i]
+                open_list.append(node_tmp)
+                for open_node in open_list[:len(open_list)-1]:
+                    if (open_node.nums_table == node_tmp.nums_table).all():
+                        if open_node.move_count > node_tmp.move_count:
+                            open_list.remove(open_node)
+                        else:
+                            open_list.remove(node_tmp)
+                            pass
                         break
-                    if (node_tmp.nums_table == node_n_move[i].nums_table).all():
-                        no_exist = False
-                    node_tmp = node_tmp.last_node
+                for closed_node in closed_list:
+                    if (closed_node.nums_table == node_tmp.nums_table).all():
+                        if closed_node.move_count < node_tmp.move_count:
+                            if open_list.count(node_tmp) != 0:
+                                open_list.remove(node_tmp)
+        
+        # for i in range(len(node_n_move)):
+        #     if move_succeed[i]:
+        #         no_exist = True
+        #         node_tmp = node_n.last_node
+        #         while True:
+        #             if node_tmp == None:
+        #                 break
+        #             if (node_tmp.nums_table == node_n_move[i].nums_table).all():
+        #                 no_exist = False
+        #             node_tmp = node_tmp.last_node
 
-                if no_exist:
-                    open_list.append(node_n_move[i])
+                # if no_exist:
+                #     open_list.append(node_n_move[i])
 
         
-        
+        open_list.sort(key = node_compare)
+        print('@' + str(len(open_list)))
+        # string = ' '
+        # for nodess in open_list:
+        #     string += str(nodess.score)
+        #     string += ' '
+        # print(string)
+
+
+def main():
+    r_num = 3
+    c_num = 3
+    nums = []
+    for i in range(r_num * c_num):
+        nums.append(i)
+    
+    random.shuffle(nums)
+    start_nums_table = np.array(nums).reshape(r_num,c_num)
+    start_nums_table = np.array(
+        [[2,8,3],
+        [1,6,4],
+        [7,0,5]]
+    )
+    print('start_nums_table:')
+    print(start_nums_table)
+
+    random.shuffle(nums)
+    end_nums_table = np.array(nums).reshape(r_num,c_num)
+    end_nums_table = np.array(
+        [[8,0,3],
+        [2,1,4],
+        [7,6,5]]
+    )
+    print('end_nums_table:')
+    print(end_nums_table)
+    hgs(start_nums_table,end_nums_table)
                 
-
+main()
--- hgs_24nums/hgs_24nums_node.py
+++ hgs_24nums/hgs_24nums_node.py
@@ -2,38 +2,48 @@ import numpy as np
 class HGS24NumsNode(object):
     start_nums_table = ''
     end_nums_table = ''
+    r_num = 3
+    c_num = 3
     '''
     启发式搜索算法的节点
     '''
-    def __init__(self, param, type):
+    def __init__(self, param, type = 1):
         '''
         nums_table: 数码表
         '''
         if type == 0:
+            print('start')
             self.nums_table = np.array(param)
             self.score = 0
-            self.calc_graph_score()
             self.last_move = ''
             self.blank_location = self.get_blank_location()
-            self.last_node = 0
+            self.last_node = None
+            self.move_count = 0
+            self.calc_graph_score()
         else:
             self.nums_table = np.array(param.nums_table)
             self.score = param.score
             self.last_move = param.last_move
             self.blank_location = [param.blank_location[0],param.blank_location[1]]
             self.last_node = param
+            self.move_count = param.move_count
 
 
     def get_graph_score(self):
         return self.score
 
     def calc_graph_score(self):
-        print(" ")
+        h = 0
+        for i in range(HGS24NumsNode.r_num):
+            for j in range(HGS24NumsNode.c_num):
+                if self.nums_table[i][j] != HGS24NumsNode.start_nums_table[i][j]:
+                    h += 1
+        self.score = self.move_count + h
 
     def get_blank_location(self):
-        for i in range(5):
-            for j in range(5):
-                if self.nums_table[i][j] == -1:
+        for i in range(HGS24NumsNode.r_num):
+            for j in range(HGS24NumsNode.c_num):
+                if self.nums_table[i][j] == 0:
                     return [i,j]
         return [0,0]
 
@@ -47,11 +57,14 @@ class HGS24NumsNode(object):
         x = self.blank_location[0]
         y = self.blank_location[1]
         self.nums_table[x][y] = self.nums_table[x][y - 1]
-        self.nums_table[x][y - 1] =-1
+        self.nums_table[x][y - 1] =0
 
         self.last_move = 'up'
         self.blank_location = [x, y - 1]
-        self.calc_graph_score
+        
+        self.move_count += 1
+
+        self.calc_graph_score()
         return True
 
     def blank_down(self):
@@ -59,16 +72,17 @@ class HGS24NumsNode(object):
         数码表空位置下移
         若不能移则返回NULL
         '''
-        if self.last_move == 'up' or self.blank_location[1] == 4:
+        if self.last_move == 'up' or self.blank_location[1] == HGS24NumsNode.c_num - 1:
             return False
         x = self.blank_location[0]
         y = self.blank_location[1]
         self.nums_table[x][y] = self.nums_table[x][y + 1]
-        self.nums_table[x][y + 1] =-1
+        self.nums_table[x][y + 1] =0
 
         self.last_move = 'down'
         self.blank_location = [x, y + 1]
-        self.calc_graph_score
+        self.move_count += 1
+        self.calc_graph_score()
         return True
 
     def blank_right(self):
@@ -76,16 +90,17 @@ class HGS24NumsNode(object):
         数码表空位置右移
         若不能移则返回NULL
         '''
-        if self.last_move == 'left' or self.blank_location[0] == 4:
+        if self.last_move == 'left' or self.blank_location[0] == HGS24NumsNode.r_num -1:
             return False
         x = self.blank_location[0]
         y = self.blank_location[1]
         self.nums_table[x][y] = self.nums_table[x + 1][y]
-        self.nums_table[x + 1][y] =-1
+        self.nums_table[x + 1][y] =0
 
         self.last_move = 'right'
         self.blank_location = [x + 1, y]
-        self.calc_graph_score
+        self.move_count += 1
+        self.calc_graph_score()
         return True
 
     def blank_left(self):
@@ -98,9 +113,10 @@ class HGS24NumsNode(object):
         x = self.blank_location[0]
         y = self.blank_location[1]
         self.nums_table[x][y] = self.nums_table[x - 1][y]
-        self.nums_table[x - 1][y] =-1
+        self.nums_table[x - 1][y] =0
 
         self.last_move = 'left'
         self.blank_location = [x - 1, y]
-        self.calc_graph_score
+        self.move_count += 1
+        self.calc_graph_score()
         return True
